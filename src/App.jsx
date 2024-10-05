32762b557809ee912cfff85dc98a7ee4238f0b29
Rory Goddard <goddardrory@gmail.com>
2024-10-05 14:22:19 +0200

    Fix: Relative path links

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
-import Vans from "./components/Vans.jsx"
-import VanDetail from "./components/VanDetail.jsx"
+import Vans from "./pages/Vans/Vans.jsx"
+import VanDetail from "./pages/Vans/VanDetail.jsx"
 import Layout from "./components/Layout.jsx"
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
@@ -14,16 +14,16 @@ function App() {
   return (
     <>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
           
-          <Route path="/host" element={<HostLayout />}>
-            <Route path="/host" element={<Dashboard />} />
-            <Route path="/host/income" element={<Income />} />
-            <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
       </Routes>
