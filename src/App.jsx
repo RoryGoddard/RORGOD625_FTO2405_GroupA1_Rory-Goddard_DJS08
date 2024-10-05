6de623925f4636af8471372d60b6ef4678357eef
Rory Goddard <goddardrory@gmail.com>
2024-10-05 14:11:07 +0200

    Fix: Refactor navbar to header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import Home from "./components/Home.jsx"
 import About from "./components/About.jsx"
-import Navbar from "./components/Navbar.jsx"
 import Vans from "./components/Vans.jsx"
 import VanDetail from "./components/VanDetail.jsx"
+import Layout from "./components/Layout.jsx"
 import { Route, Routes } from "react-router-dom"
 
 function App() {
 
   return (
     <>
-      <Navbar />
       <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/about" element={ <About /> } />
-        <Route path="/vans" element={ <Vans /> } />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/about" element={ <About /> } />
+          <Route path="/vans" element={ <Vans /> } />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </>
 
