d1e58f4e909fe4a73c84480a8abc2b42e2c7587e
Rory Goddard <goddardrory@gmail.com>
2024-10-05 15:08:37 +0200

    Fix: Install react icons

--- src/api.js
+++ src/api.js
@@ -11,14 +11,14 @@ import {
 } from "firebase/firestore/lite"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCpO40q2wqHPYcvfCiQY8cbfJtczEN0bs0",
-    authDomain: "vanlife-58e01.firebaseapp.com",
-    projectId: "vanlife-58e01",
-    storageBucket: "vanlife-58e01.appspot.com",
-    messagingSenderId: "379855849054",
-    appId: "1:379855849054:web:3ebf3cb2817aa040fe16cf"
-  };
-  
+    apiKey: "AIzaSyD_k3v3HK3tKEqhlqFHPkwogW7PqEqhGhk",
+    authDomain: "vanlife-a1af5.firebaseapp.com",
+    projectId: "vanlife-a1af5",
+    storageBucket: "vanlife-a1af5.appspot.com",
+    messagingSenderId: "803007000356",
+    appId: "1:803007000356:web:446cd3a1ca406839258db1"
+};
+
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 
@@ -52,36 +52,6 @@ export async function getHostVans() {
     }))
     return vans
 }
-
-/* 
-This 👇 isn't normally something you'd need to do. Instead, you'd 
-set up Firebase security rules so only the currently logged-in user 
-could edit their vans.
-
-https://firebase.google.com/docs/rules
-
-I'm just leaving this here for educational purposes, as it took
-me a while to find the `documentId()` function that allows you
-to use a where() filter on a document's ID property. (Since normally
-it only looks at the data() properties of the document, meaning you
-can't do `where("id", "==", id))`
-
-It also shows how you can chain together multiple `where` filter calls
-*/
-
-// export async function getHostVan(id) {
-//     const q = query(
-//         vansCollectionRef,
-//         where(documentId(), "==", id),
-//         where("hostId", "==", "123")
-//     )
-//     const snapshot = await getDocs(q)
-//     const vans = snapshot.docs.map(doc => ({
-//         ...doc.data(),
-//         id: doc.id
-//     }))
-//     return vans[0]
-// }
 
 export async function loginUser(creds) {
     const res = await fetch("/api/login",
