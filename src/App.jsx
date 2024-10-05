c9ccb812b2a0ae6210a8b99b271172b343becbbd
Rory Goddard <goddardrory@gmail.com>
2024-10-05 14:57:38 +0200

    Fix: Refactor to use firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,52 @@
-import Home from "./pages/Home.jsx"
-import About from "./pages/About.jsx"
-import Vans from "./pages/Vans/Vans.jsx"
-import VanDetail from "./pages/Vans/VanDetail.jsx"
-import Layout from "./components/Layout.jsx"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans from "./pages/Vans/Vans"
+import VanDetail from "./pages/Vans/VanDetail"
+import Login from "./pages/Login"
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
+import HostVans from "./pages/Host/HostVans"
+import HostVanDetail from "./pages/Host/HostVanDetail"
+import HostVanInfo from "./pages/Host/HostVanInfo"
+import HostVanPricing from "./pages/Host/HostVanPricing"
+import HostVanPhotos from "./pages/Host/HostVanPhotos"
+import NotFound from "./pages/NotFound"
+import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
+import AuthRequired from "./components/AuthRequired"
 import { Route, Routes } from "react-router-dom"
 
 function App() {
 
   return (
     <>
-      <Routes>
+       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
-          
-          <Route path="host" element={<HostLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="income" element={<Income />} />
-            <Route path="reviews" element={<Reviews />} />
+          <Route
+            path="login"
+            element={<Login />}
+          />
+
+          <Route element={<AuthRequired />}>
+            <Route path="host" element={<HostLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo />} />
+                <Route path="pricing" element={<HostVanPricing />} />
+                <Route path="photos" element={<HostVanPhotos />} />
+              </Route>
+            </Route>
           </Route>
+
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </>
