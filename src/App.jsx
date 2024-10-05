76dd7b111f36bcab4de6941a2bd987e5baf17b19
Rory Goddard <goddardrory@gmail.com>
2024-10-05 14:02:20 +0200

    Feat: console log van params object

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Home from "./components/Home.jsx"
 import About from "./components/About.jsx"
 import Navbar from "./components/Navbar.jsx"
 import Vans from "./components/Vans.jsx"
+import VanDetail from "./components/VanDetail.jsx"
 import { Route, Routes } from "react-router-dom"
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={ <Home /> } />
         <Route path="/about" element={ <About /> } />
         <Route path="/vans" element={ <Vans /> } />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </>
 
---
+++ src/components/VanDetail.jsx
@@ -0,0 +1,10 @@
+import { useParams } from "react-router-dom"
+export default function VanDetail() {
+    const params = useParams()
+    console.log(params)
+    /**
+     * Challenge part 1:
+     * Re-write what I just wrote and console log the params object
+     */
+    return <h1>Van detail page goes here</h1>
+}
